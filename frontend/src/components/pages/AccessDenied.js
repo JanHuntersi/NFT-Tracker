674b0f8f3de7d273a18e429b9ff17ee3cb729df6
= <jan.sernec@gmail.com>
2022-02-05 01:03:58 +0100

    Cleaned up AccesDenied.js and NotFoundPage.js

--- frontend/src/components/pages/AccessDenied.js
+++ frontend/src/components/pages/AccessDenied.js
@@ -1,14 +1,25 @@
-import { Link as UiLink } from "@chakra-ui/react";
+import {
+	GridItem,
+	Center,
+	Heading,
+	Link as UiLink,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import Layout from "../layout/Layout";
 
-
-export default function AccessDenied(){
-    return(
-        <>        <p>acces was denied!</p>
-        <Link to="/">
-            <UiLink> Back to main page</UiLink>
-        </Link>
-        </>
-
-        );
+export default function AccessDenied() {
+	return (
+		<Layout>
+			<GridItem colStart={1} colSpan={3} row>
+					<Heading as="h1" mt="6" mb="6" textAlign="center">
+						Access was denied
+					</Heading>
+					<Center>
+						<Link to="/">
+							<UiLink> Back to main page</UiLink>
+						</Link>
+					</Center>		
+			</GridItem>
+		</Layout>
+	);
 }
--- frontend/src/components/pages/NotFoundPage.js
+++ frontend/src/components/pages/NotFoundPage.js
@@ -1,23 +1,26 @@
-import { Link, Navigate } from "react-router-dom";
-import { Link as UiLink, Heading, GridItem } from "@chakra-ui/react";
-export default function NotFoundPage(){
-
-
-function goToPreviusPage(){
-
-  Navigate(-1);
-}
-  return(
-    <GridItem
-    colStart={0.5}
-    colSpan={3}
-    p={6}
-    >
-       <Heading as="h1" mt="3" mb="6">
-         404 PAGE NOT FOUND
-           </Heading> 
-                      <UiLink onClick={goToPreviusPage}> Back to main page</UiLink>
-        
-    </GridItem>
-);
+import { useNavigate } from "react-router-dom";
+import { Heading, GridItem, Button, Center } from "@chakra-ui/react";
+export default function NotFoundPage() {
+	let navigate = useNavigate();
+	function goToPreviusPage() {
+		navigate(-1);
+	}
+	return (
+		<GridItem colStart={1} colSpan={3} p={6}>
+			<Heading as="h1" textAlign="center" mt="3" mb="6">
+				404 PAGE NOT FOUND
+			</Heading>
+			<Center>
+				<Button
+					colorScheme="blue"
+					position="center"
+					mt="4"
+					size="md"
+					onClick={goToPreviusPage}
+				>
+					Back to previus page
+				</Button>
+			</Center>
+		</GridItem>
+	);
 }

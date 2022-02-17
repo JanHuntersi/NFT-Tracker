26fe3ec9ebe7238e583ce689a1630704e5b85de2
= <jan.sernec@gmail.com>
2022-02-17 14:02:43 +0100

    Switched to MoralisApi

--- frontend/src/components/nft/NftTest.js
+++ frontend/src/components/nft/NftTest.js
@@ -1,7 +1,62 @@
+import { Button, CircularProgress, FormControl, FormLabel,Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { useNFTBalances } from "react-moralis";
+
+
+
 export default function NftTest(){
+ 
+
+    const { getNFTBalances, data, error } = useNFTBalances();
+    const [nftAddress,setAddress] = useState(null)
+    const [isLoading,setLoading] = useState(false)
+
+    function  handleSubmit(e){
+        e.preventDefault();
+        setLoading(true);
+        console.log(`Value je ${nftAddress}`);
+        getNFTBalances({ params: { address:nftAddress },
+        onSuccess:() =>{ console.log("delas")},
+        onError:() =>{console.log("napak bila")},
+        onComplete:() => {setLoading(false)}
+        
+     })
+    }
+
     return(
-        <>
-        <p>zdravo</p>
-        </>
+        <div>
+        {error && <>{"Napaka"}</>}
+        
+<p>0x67425e833b3ba8970636d5fb18134487f52aac59</p>
+        <form onSubmit={handleSubmit}>
+        <FormControl isRequired>
+        <FormLabel> Enter Users Address</FormLabel>
+        <Input
+        type="text"
+        placeholder="enter address here"
+        size="lg"
+        onChange={event => setAddress(event.currentTarget.value)}>
+        </Input>
+        <Button
+        variant="outline"
+        type="submit"
+        >
+            {isLoading ? (
+                <CircularProgress
+                isIndeterminate
+                size="24px"
+                color="teal"
+                />
+            ): (
+                "Fetch!"
+            )}
+        </Button>
+        </FormControl>
+        </form>
+
+
+       {data && data.total!=0 && <>{data.total}</>}
+        <p>{data && data.total==0 && <>Slabi si</> }</p>
+      </div>
     )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 import { AuthProvider } from './hooks/useAuth';
-
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
- <ChakraProvider>
+    <MoralisProvider appId="mTk0bxf7T5quoQURocD1jT1EnE6KoBh5wDJc6nUD" serverUrl="https://51rapn7enz8b.usemoralis.com:2053/server">
+    <ChakraProvider>
   <AuthProvider>
     <App />
     </AuthProvider>
  </ChakraProvider>
-   
-
+  
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
